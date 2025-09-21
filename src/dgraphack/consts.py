5d9c4eb20582ec7575785b662612f23982e9b47e
David Vaughan <david@davidv.xyz>
2025-09-21 16:00:05 -0700

    Change port

--- src/dgraphack/consts.py
+++ src/dgraphack/consts.py
@@ -3,7 +3,7 @@ import tempfile
 
 
 API_HOST = "0.0.0.0"
-API_PORT = 8123
+API_PORT = 8124
 API_URL = f"http://{API_HOST}:{API_PORT}"
 API_WORK_DIR = os.path.join(tempfile.gettempdir(), "dgraphack")
 API_IMG_DIR = os.path.join(API_WORK_DIR, "imgs")
