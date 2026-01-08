c803f9d4c4c1b178cbe0058d5c0dfce2df25db08
David Vaughan <david@davidv.xyz>
2026-01-08 14:31:35 -0800

    Replace API_HOST 0.0.0.0 -> 127.0.0.1

--- src/dgraphack/consts.py
+++ src/dgraphack/consts.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 
-API_HOST = "0.0.0.0"
+API_HOST = "127.0.0.1"
 API_PORT = 8124
 API_URL = f"http://{API_HOST}:{API_PORT}"
 API_WORK_DIR = os.path.join(tempfile.gettempdir(), "dgraphack")
